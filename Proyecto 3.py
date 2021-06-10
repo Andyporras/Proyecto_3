5117c6f6dc92d1e585d3f794bc1e7a8e130ce399
Andy Porras <andyporras6@gmail.com>
2021-06-10 15:56:00 -0600

    Punto b del Proyecto

--- Proyecto 3.py
+++ Proyecto 3.py
@@ -2,8 +2,36 @@
 import tkinter
 from tkinter import*
 
-#Si puedo editar
+
 def menu():
     ventana=Tk()
+    ventana.geometry("400x400")
+    entry=tkinter.Entry(ventana,text="",
+                        font=("Times New Roman",14),
+                        bg="white", fg="Black")
+    entry.pack()
+    etiqueta=tkinter.Label(ventana,text="Ingrese su nombre Completo",
+                           font=("Times New Roman",14),
+                           bg="white", fg="Black").pack()
+    entry=tkinter.Entry(ventana,text="",
+                        font=("Times New Roman",14),
+                        bg="white", fg="Black")
+    entry.pack()
+    etiqueta=tkinter.Label(ventana,text="Ingrese su nombre de usuario",
+                           font=("Times New Roman",14),
+                           bg="white", fg="Black").pack()
+    entry=tkinter.Entry(ventana,text="",
+                        font=("Times New Roman",14),
+                        bg="white", fg="Black")
+    entry.pack()
+    etiqueta=tkinter.Label(ventana,text="Ingrese su contraseña",
+                           font=("Times New Roman",14),
+                           bg="white", fg="Black").pack()
+    
+    boton=tkinter.Button(ventana,text="Continuar",font=("Times New Roman",14),
+                         bg="white", fg="Black")
+    boton.pack()
+    
+    
 
 menu()
