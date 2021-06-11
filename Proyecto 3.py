4aea7ac311100e044e1e5e13783cabfe58464119
Andy Porras <andyporras6@gmail.com>
2021-06-10 21:21:38 -0600

    funcionalidad del menu principal
    funcionalidad y validaciones

--- Proyecto 3.py
+++ Proyecto 3.py
@@ -1,6 +1,7 @@
 
 import tkinter
 from tkinter import*
+from tkinter import messagebox
 
 
 def menu():
@@ -18,30 +19,48 @@ def menu():
     tkinter.Label(ventana, text="- - -  - - -  - - -  - - -  - - -  - - -  - - -  - - -  - - -  - - -  - - -" ,
                   font=("Arial Black",12),bg="SteelBlue3",fg="Black").place(x=10,y=100)
 
-    entry=tkinter.Entry(ventana,text="",
+    entry1=tkinter.Entry(ventana,text="",
                         font=("Times New Roman",14),
                         bg="SteelBlue1", fg="Black")
-    entry.place(x=105,y=160)
+    entry1.place(x=105,y=160)
     etiqueta=tkinter.Label(ventana,text="Ingrese su nombre Completo:",
                            font=("Times New Roman",14),
                            bg="SteelBlue3", fg="Black").place(x=85,y=130)
-    entry=tkinter.Entry(ventana,text="",
+    entry2=tkinter.Entry(ventana,text="",
                         font=("Times New Roman",14),
                         bg="SteelBlue1", fg="Black")
-    entry.place(x=105,y=220)
+    entry2.place(x=105,y=220)
     etiqueta=tkinter.Label(ventana,text="Ingrese su nombre de Usuario:",
                            font=("Times New Roman",14),
                            bg="SteelBlue3", fg="Black").place(x=85,y=190)
-    entry=tkinter.Entry(ventana,text="",
+    entry3=tkinter.Entry(ventana,text="",show="*",
                         font=("Times New Roman",14),
                         bg="SteelBlue1", fg="Black")
-    entry.place(x=105,y=280)
+    entry3.place(x=105,y=280)
     etiqueta=tkinter.Label(ventana,text="Ingrese su Contraseña:",
                            font=("Times New Roman",14),
                            bg="SteelBlue3", fg="Black").place(x=111,y=250)
+    def controlDeAcceso():
+        nombre=entry1.get()
+        usuario=entry2.get()
+        contraseña=entry3.get()
+        archivo=open("acceso.txt")
+        datos=archivo.readlines()
+        if(nombre+"\n")in datos:
+            indice=datos.index(nombre+"\n")
+            if(usuario+"\n")== datos[indice+1]:
+                if(contraseña+"\n")==datos[indice+2]:
+                    ventana.destroy()
+                else:
+                    messagebox.showerror("error","Contraseña incorrecta") 
+            else:
+                 messagebox.showerror("error","Usuario incorrecto")
+        else:
+             messagebox.showerror("error","Nombre incorrect0")
+            
     
     boton=tkinter.Button(ventana,text="Continuar",font=("Times New Roman",14),
-                         bg="DeepSkyBlue4", fg="Black")
+                         bg="DeepSkyBlue4", fg="Black",command=controlDeAcceso)
     boton.place(x=160,y=320)
     
     
